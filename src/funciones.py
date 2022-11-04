4d9e998cc078e68d1867ea38f23fd72acd68d12e
Sergio Suarez <superen2000@yahoo.es>
2022-11-04 19:25:00 +0100

    Actualizacion

---
+++ src/funciones.py
@@ -0,0 +1,41 @@
+def limpiar_fecha(df):
+    for i in df.last_update.values:
+        df['fecha']=i[:11]
+    
+    for i in df.last_update.values:
+        df['hora']=i[11:] 
+        
+    
+    
+def special_features(x):
+    trail=[]
+    commen=[]
+    bh_scene=[]
+    del_scene=[]
+    if 'Trailers' in x:
+        trail.append('Y')
+    else:
+        trail.append('N')
+    if 'Commentaries' in x:
+        trail.commen('Y')
+    else:
+        commen.append('N')
+    if 'Behind the Scenes' in x:
+        bh_scene.append('Y')
+    else:
+        bh_scene.append('N')
+    if 'Deleted Scenes' in x:
+        del_scene.append('Y')
+    else:
+        del_scene.append('N')
+    
+    print(trail)
+    print(commen)
+    print(bh_scene)
+    print(del_scene)
+        
+        
+        
+        
+        
+        
